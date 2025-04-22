80dca4bb3173d70326179ead47462f46161f7614
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-04-22 15:36:36 -0500

    commit2

---
+++ fundamentos.js
@@ -0,0 +1,25 @@
+// Variable: contenedor (caja) de informacion.
+// ES6
+
+// let - const
+// var (no usaremos) - hosting
+
+// Tipo de datos primitivos
+let numOrden = 127 //int
+let modulo = "Mis ordenes" //string
+let status = true //booleano
+let ordenes = ["Balon", "Camiseta", "Zapatos"] //array
+let tipoPago = "E" //char
+let precioFinal = 155.99 //float
+const CI = 1753078839 //int - inmutable
+
+// Tipo de datos compuestos 
+let ordenes = ["Blaon", "Camiseta", "Zapatos"] //array
+let detalleOrden = {
+    id: 127,
+    nombre: "Mis ordenes",
+    status: true,
+    tipoPago: "E",
+    precioFinal: 155.99,
+    items: ["Balon", "Camiseta", "Zapatos"]
+} //object
