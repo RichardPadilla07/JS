94d0c44d1c5ebda32d26684cd8ef860a0ffd194f
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-04-22 15:44:39 -0500

    commit 3

--- fundamentos.js
+++ fundamentos.js
@@ -14,12 +14,27 @@ let precioFinal = 155.99 //float
 const CI = 1753078839 //int - inmutable
 
 // Tipo de datos compuestos 
-let ordenes = ["Blaon", "Camiseta", "Zapatos"] //array
-let detalleOrden = {
+let ordenes2 = ["Blaon", "Camiseta", "Zapatos"] //array
+let detalleOrden2 = {
     id: 127,
-    nombre: "Mis ordenes",
-    status: true,
-    tipoPago: "E",
-    precioFinal: 155.99,
+    estado: "Activo",
+    precio: 155.99,
     items: ["Balon", "Camiseta", "Zapatos"]
 } //object
+
+// TAREA desafio: json vs objecto
+let ordenes3 = ["Blaon", "Camiseta", "Zapatos"] //array
+let detalleOrden3 = {
+    "id": 127,
+    "estado": "Activo",
+    "precio": 155.99,
+    "items": ["Balon", "Camiseta", "Zapatos"]
+} //object
+
+
+// Strings
+let nombreCompleto = "Richard Padilla"
+nombreCompleto.length //longitud
+nombreCompleto.toUpperCase() // Mayusculas
+nombreCompleto.toLowerCase() // Minusculas
+nombreCompleto.trim() // Eliminar espacios en blanco
