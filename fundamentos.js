984ad605d8866a4783a07e902fdd4addfa985dc4
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-04-22 16:16:17 -0500

    commit 4

--- fundamentos.js
+++ fundamentos.js
@@ -38,3 +38,50 @@ nombreCompleto.length //longitud
 nombreCompleto.toUpperCase() // Mayusculas
 nombreCompleto.toLowerCase() // Minusculas
 nombreCompleto.trim() // Eliminar espacios en blanco
+nombreCompleto.charAt(0) // Obtener el primer caracter
+nombreCompleto.includes("Richard") // Verificar si existe un texto
+
+
+//concatenacion
+//console.log(nombreCompleto.length) //longitud  
+//console.log(nombreCompleto.trim())
+
+//ES6 - Template String
+console.log(`Bienvenido - ${nombreCompleto}`) 
+
+// Numbers
+let cuotas = "6"
+console.log(cuotas + 3)
+
+// ES6 convertir de string a numeeros
+console.log(+cuotas + 3)
+
+// Muestra el tipo de dato
+console.log(typeof(cuotas)) 
+
+// Orden de presedencia de los operadores
+console.log(4*3**2)  
+
+// Booleanos
+let saldoUsuario = 1000
+let saldoTarjeta = "1000"
+
+// ES6 - Comparacion estricta
+// Comparacion de tipos de datos y valores
+console.log(saldoUsuario == saldoTarjeta)      
+
+// Condicionales
+// IF
+
+const carritoCompra = []
+if (carritoCompra.length === 0) {
+    console.log("El carrito de compras esta vacio")
+}
+else {
+    console.log("El carrito de compras tiene productos") 
+} 
+
+// ES6 - OPERADOR TERNARIO                               // V                                    // F
+const mensaje = carritoCompra.length === 0 ? "El carrito de compras esta vacio" : "El carrito de compras tiene productos"
+console.log(mensaje) 
+
